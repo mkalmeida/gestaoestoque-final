4e307dd74176fb2fa789c93f984a4f272e6d03ab
mkalmeida <michaellasfalmeida@gmail.com>
2023-05-11 16:03:03 -0300

    funcionando modelo e interface. Postman ok

--- src/main/java/com/techafropretas/gestaoestoque/controller/NotafiscalController.java
+++ src/main/java/com/techafropretas/gestaoestoque/controller/NotafiscalController.java
@@ -1,5 +1,31 @@
 package com.techafropretas.gestaoestoque.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/nota")
 public class NotafiscalController {
 
+	//inicio
+	@RequestMapping(value="", method=RequestMethod.GET)
+	public @ResponseBody String inicio() {
+		return "inicio nota";
+	}
+	//sobre
+	@RequestMapping(value="/sobre", method=RequestMethod.GET)
+	public @ResponseBody String sobre() {
+		return "sobre nota";
+	}
+	
+	//contato
+	@RequestMapping(value="/contato", method=RequestMethod.GET)
+	public @ResponseBody String contato() {
+		return "contato nota";
+	}
+	
 }
+
--- src/main/java/com/techafropretas/gestaoestoque/controller/ProdutoController.java
+++ src/main/java/com/techafropretas/gestaoestoque/controller/ProdutoController.java
@@ -1,5 +1,29 @@
 package com.techafropretas.gestaoestoque.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/produto")
 public class ProdutoController {
 
+	//inicio
+		@RequestMapping(value="", method=RequestMethod.GET)
+		public @ResponseBody String inicio() {
+			return "inicio prod";
+		}
+		//sobre
+		@RequestMapping(value="/sobre", method=RequestMethod.GET)
+		public @ResponseBody String sobre() {
+			return "sobre prod";
+		}
+		
+		//contato
+		@RequestMapping(value="/contato", method=RequestMethod.GET)
+		public @ResponseBody String contato() {
+			return "contato prod";
+		}
 }
